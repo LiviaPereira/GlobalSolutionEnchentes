151f6ca4c1c236a2da4936eb8d3aaa33885d0ed2
livia.correa <livia.correa@fiap.com.br>
2025-05-30 20:28:54 -0300

    Header, footer, sessão inicial

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnAlertar");
+  btn.addEventListener("click", () => {
+    alert("Notificações ativadas com sucesso! Você será avisado sobre enchentes na sua região.");
+  });
+});
